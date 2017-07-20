5e8699eb541bf51705d7c41651e9a8466b8eaf1b
Tim Culleton <mit_notelluc@hotmail.com>
2017-07-20 19:55:54 +1000

    Small change to initialize the Anime class using IdirData. More work to be done tomorrow at work.. it be Friday :P

--- src/services/animeServices/anime.ts
+++ src/services/animeServices/anime.ts
@@ -3,15 +3,32 @@ import {IDirData} from "../directoryServices/dirTypes"
 
 import path = require("path");
 
+
+const REGEX_VIDEO_CHECK = /\.avi$|\.mkv$|\.mp4$/;
+const SEASON = "Season";
+const TV_SHOW_NFO = "tvshow.nfo";
+
+
 export class Anime implements IAnime {
+
+    name: string;
+    seasons: Map<string, string[]>;
+    tvdbId: string;
+    private rootPath: string;
+
 
     constructor(dirData: IDirData) {
         this.name = path.basename(dirData.root);
-    }
+        this.rootPath = dirData.root;
 
-    name: string;
-    seasons: Map<string, IDirData>;
-    tvdbId: string;
+        if(dirData.dirs.length === 0) {
+            this.seasons.set(Anime.createSeasonName(1), Anime.createEpisodeList(dirData))
+        } else {
+            dirData.dirs.forEach((season, index) => {
+                this.seasons.set(Anime.createSeasonName(index + 1), Anime.createEpisodeList(season));
+            })
+        }
+    }
 
     areEpisodesInKodiFormat(): boolean {
         return null;
@@ -20,4 +37,20 @@ export class Anime implements IAnime {
     updateEpisodeNames() {
     }
 
+
+    private static createSeasonName(seasonNum: number): string {
+        return `${SEASON} ${seasonNum}`;
+    }
+
+    private static createEpisodeList(season: IDirData): string[] {
+        let episodes: string[] = [];
+
+        for(let episode of season.files) {
+            if(REGEX_VIDEO_CHECK.test(episode)) {
+                episodes.push(path.relative(season.root, episode));
+            }
+        }
+
+        return episodes;
+    }
 }
--- src/services/animeServices/animeTypes.ts
+++ src/services/animeServices/animeTypes.ts
@@ -2,7 +2,7 @@ import {IDirData} from "../directoryServices/dirTypes";
 
 export interface IAnime {
     name: string;
-    seasons: Map<string, IDirData>;
+    seasons: Map<string, string[]>;
 
     tvdbId: string;
     areEpisodesInKodiFormat(): boolean;
