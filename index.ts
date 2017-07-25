33fec0e3a95684ba3a8224c7ea0311e6b43b6e70
Tim Culleton <mit_notelluc@hotmail.com>
2017-07-25 23:05:35 +1000

    Anime auto renamer working :D

--- index.ts
+++ index.ts
@@ -3,9 +3,21 @@
  */
 
 import {DirService} from "./src/services/directoryServices/getDirContentsService";
+import {Anime} from "./src/services/animeServices/anime"
 
-DirService.getDirContents("E:\\Anime").then(dirData => {
+DirService.getDirContents("E:\\Anime\\ID-0").then(dirData => {
     console.log(`Data recieved :D`);
+    let id0 = new Anime(dirData);
+
+    if(!id0.areEpisodesInKodiFormat()) {
+        id0.updateEpisodeNames().then(files => {
+            for(let file of files) {
+                console.log(`renamed: ${file}`);
+            }
+        })
+    }
+
+    console.log(`Episodes are in Kodi format: ${id0.areEpisodesInKodiFormat()}`);
     return 0;
 }, (error) => {
     console.error(error);
--- src/services/animeServices/anime.ts
+++ src/services/animeServices/anime.ts
@@ -2,9 +2,13 @@ import {IAnime} from "./animeTypes"
 import {IDirData} from "../directoryServices/dirTypes"
 
 import path = require("path");
+import fs = require("fs");
 
 
 const REGEX_VIDEO_CHECK = /\.avi$|\.mkv$|\.mp4$/;
+//Tests normal format '1x01' and S0E01 format
+const REGEX_KODI_FORMAT = /^\d+x\d+|S\d+E\d+/;
+const REGEX_EXTRACT_EP_NUM = /\s(\d+)\s|_(\d+)_|-\s(\d+)/;
 const SEASON = "Season";
 const TV_SHOW_NFO = "tvshow.nfo";
 
@@ -20,9 +24,11 @@ export class Anime implements IAnime {
     constructor(dirData: IDirData) {
         this.name = path.basename(dirData.root);
         this.rootPath = dirData.root;
+        this.seasons = new Map<string, string[]>();
 
         if(dirData.dirs.length === 0) {
-            this.seasons.set(Anime.createSeasonName(1), Anime.createEpisodeList(dirData))
+            let movedFiles = this.moveFilesToSeason(1, Anime.createEpisodeList(dirData));
+            this.seasons.set(Anime.createSeasonName(1), movedFiles)
         } else {
             dirData.dirs.forEach((season, index) => {
                 this.seasons.set(Anime.createSeasonName(index + 1), Anime.createEpisodeList(season));
@@ -31,13 +37,124 @@ export class Anime implements IAnime {
     }
 
     areEpisodesInKodiFormat(): boolean {
-        return null;
+        let kodiFormat = true;
+        for(let season of this.seasons.values()) {
+            for(let episode of season) {
+                if(!REGEX_KODI_FORMAT.test(episode)) {
+                    return false;
+                }
+            }
+        }
+        return kodiFormat;
     }
 
-    updateEpisodeNames() {
+    /**
+     * Updates the anime collection prefxing the files with a season/episode tag
+     * to assist in kodi scraping the meta data.
+     *
+     * This will return a promise containing a collection files that have been updated.
+     * @async
+     * @returns {Promise<string[]>}
+     */
+    updateEpisodeNames(): Promise<string[]> {
+
+        return new Promise<string[]>((resolve, reject) => {
+
+            if(!this.areEpisodesInKodiFormat()) {
+                let seasonNum = 0;
+                let promises = [];
+                for(let [seasonPath, files] of this.seasons.entries()) {
+                    ++seasonNum;
+
+                    for(let file of files) {
+                        let episode = path.basename(file);
+                        if(!REGEX_KODI_FORMAT.test(episode)) {
+                            let candidates = REGEX_EXTRACT_EP_NUM.exec(episode);
+                            let epNum: number = null;
+                            for(let canidate of candidates) {
+                                epNum = parseInt(canidate, 10);
+
+                                if(!isNaN(epNum)) {
+                                    break;
+                                }
+                            }
+
+                            if(!isNaN(epNum)) {
+                                promises.push(new Promise<string>((resolve, reject) => {
+                                    let newPath = path.resolve(this.rootPath, seasonPath, Anime.createEpisodePrefix(seasonNum, epNum) + episode);
+                                    fs.rename(file, newPath, (err => {
+                                        if(err) {
+                                            reject(err);
+                                        } else {
+                                            resolve(newPath);
+                                        }
+                                    }));
+                                }));
+                            }
+                        }
+                    }
+                }
+
+                Promise.all(promises).then(files => {
+                    resolve(files);
+                }, reject);
+            }
+            resolve([]);
+        });
     }
 
+    /**
+     * Moves a collection of files to a season folder.
+     * This will block while this action is performed to allow it it be called during the constructor
+     * @param {number} seasonNum
+     * @param {string[]} files
+     * @returns {string[]}
+     */
+    private moveFilesToSeason(seasonNum: number, files: string[]): string[] {
+        let movedFiles = [];
+        let seasonPath = path.resolve(this.rootPath, Anime.createSeasonName(seasonNum));
+        if(!fs.existsSync(seasonPath)) {
+            fs.mkdirSync(seasonPath);
+        }
 
+        for(let file of files) {
+            let newPath = path.resolve(seasonPath, path.basename(file));
+            fs.renameSync(file, newPath);
+            movedFiles.push(newPath);
+        }
+        return movedFiles;
+    }
+
+    /**
+     * Create a Kodi/tvdb compliant prefix: 1x05_
+     *                                    season x ep num
+     * @param {number} seasonNum
+     * @param {number} episodeNum
+     * @returns {string}
+     */
+    private static createEpisodePrefix(seasonNum: number, episodeNum: number): string {
+        return `${seasonNum}x${Anime.padNumber(episodeNum)}_`
+    }
+
+    /**
+     * Pads the number with a number of leading zeros
+     * @param {number} num
+     * @param {number} padding
+     * @returns {string}
+     */
+    private static padNumber(num: number, padding = 2): string {
+        let padString = "" + num;
+        while (padString.length < padding) {
+            padString = "0" + padString;
+        }
+        return padString;
+    }
+
+    /**
+     * Create a season folder name.
+     * @param {number} seasonNum
+     * @returns {string}
+     */
     private static createSeasonName(seasonNum: number): string {
         return `${SEASON} ${seasonNum}`;
     }
@@ -47,7 +164,7 @@ export class Anime implements IAnime {
 
         for(let episode of season.files) {
             if(REGEX_VIDEO_CHECK.test(episode)) {
-                episodes.push(path.relative(season.root, episode));
+                episodes.push(path.resolve(season.root, episode));
             }
         }
 
